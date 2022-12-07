9cc550df6de3bd3ba6a2a91101b31673bca2e682
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-12-06 20:27:04 -0500

    Initial commit

---
+++ demo/src/Team.java
@@ -0,0 +1,11 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    int played =0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+    private ArrayList<Player> members = new ArrayList<>();
+}
